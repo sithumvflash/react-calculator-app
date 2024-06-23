e9892fbf57ba24927a5c6d5e0a6caa0135e8b554
sithum vihanga <wwevihanga@gmail.com>
2024-06-24 00:08:22 +0530

    ADD DIGITS, CHOOSE OPERATION, CLEAR

--- src/App.js
+++ src/App.js
@@ -14,10 +14,27 @@ export const ACTIONS = {
 function reducer(state, { type, payload }) {
   switch (type) {
     case ACTIONS.ADD_DIGIT:
+      if (payload.digit === "0" && state.currentOperand === "0") return state;
+      if (payload.digit === "." && state.currentOperand.includes("."))
+        return state;
       return {
         ...state,
         currentOperand: '${currentOperand || ""} ${payload.digit}',
       };
+    case ACTIONS.CHOOSE_OPERATION:
+      if (state.currentOperand == null && state.previousOperand == null) {
+        return state;
+      }
+      if (state.previousOperand == null) {
+        return {
+          ...state,
+          operation: payload.operation,
+          previousOperand: state.currentOperand,
+          currentOperand: null,
+        };
+      }
+    case ACTIONS.CLEAR:
+      return {};
   }
 }
 
@@ -37,7 +54,13 @@ function App() {
         </div>
         <div className="current-operand">{currentOperand}</div>
       </div>
-      <button className="span-two">AC</button>
+      <button
+        className="span-two"
+        onClick={() => dispatch({ type: ACTIONS.CLEAR })}
+      >
+        AC
+      </button>
+
       <button id="del">DEL</button>
       {/* <button>÷</button> */}
       <OperationButton digit="÷" dispatch={dispatch} />
